bcd30f747ba83b2db8b88212ac1e3a3e5adde37a
Phan Thành Danh <155965808+thanhdanh1408@users.noreply.github.com>
2025-04-11 18:01:04 +0700

    Update DatabaseConfig.java
    edit database

--- src/main/java/com/abc/config/DatabaseConfig.java
+++ src/main/java/com/abc/config/DatabaseConfig.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/Postify?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306:3306/Postify?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root"; 
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "1234";
     private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
     public static Connection getConnection() {
